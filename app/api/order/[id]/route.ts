50627f110a4f3695745e28e28767cd3959779573
unknown <zaidkhan262523@gmil.com>
2025-05-06 12:46:38 +0530

    api erro in order

--- app/api/order/[id]/route.ts
+++ app/api/order/[id]/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
 import Order from "@/models/Order";
 
