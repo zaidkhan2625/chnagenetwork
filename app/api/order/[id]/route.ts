268980dfdac18886a6794c91cfbce2eb8a4e9f7a
unknown <zaidkhan262523@gmil.com>
2025-05-06 13:29:20 +0530

    api erro in order api

--- app/api/order/[id]/route.ts
+++ app/api/order/[id]/route.ts
@@ -1,28 +1,36 @@
-"@ts-expect-error"
+import { NextRequest, NextResponse } from "next/server";
 import { getUserFromToken } from "@/lib/auth";
 import { connectDB } from "@/lib/mongodb";
 import Order from "@/models/Order";
-import {   NextRequest, NextResponse } from "next/server";
-export async function DELETE(req:NextRequest ,{params}:{params:{id:string}}) {
+
+export async function DELETE(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
   try {
     await connectDB();
     const user = await getUserFromToken(req);
-    if(!user){
-      return NextResponse.json({ message: "user not found" }, { status: 404 });
 
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
     }
-    const { id } = params;
+
+    const { id } = context.params;
     const deletedOrder = await Order.findByIdAndDelete(id);
 
     if (!deletedOrder) {
       return NextResponse.json({ message: "Order not found" }, { status: 404 });
     }
 
-    return NextResponse.json({ message: "Order deleted", order: deletedOrder }, { status: 200 });
+    return NextResponse.json(
+      { message: "Order deleted", order: deletedOrder },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error deleting order:", error);
-    return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
   }
 }
-
-
